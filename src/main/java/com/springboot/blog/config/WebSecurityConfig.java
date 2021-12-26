8eb683f287b894a26d431b4c57557ae5240d2c9c
anfr <andriyslb@gmail.com>
2021-12-26 22:32:47 +0300

    'new'

--- src/main/java/com/springboot/blog/config/WebSecurityConfig.java
+++ src/main/java/com/springboot/blog/config/WebSecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import com.springboot.blog.service.UserService;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @Configuration
 @EnableWebSecurity
