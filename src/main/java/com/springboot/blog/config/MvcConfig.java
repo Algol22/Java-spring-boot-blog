c203e6dbc702fd2661d4e08cdc8d4718045d742b
anfr <andriyslb@gmail.com>
2022-01-04 04:27:09 +0300

    'upd'

--- src/main/java/com/springboot/blog/config/MvcConfig.java
+++ src/main/java/com/springboot/blog/config/MvcConfig.java
@@ -18,16 +18,8 @@ public class MvcConfig implements WebMvcConfigurer {
 
     //File uploads
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        exposeDirectory("postphotos", registry);
-    }
-
-    private void exposeDirectory(String dirName, ResourceHandlerRegistry registry) {
-        Path uploadDir = Paths.get(dirName);
-        String uploadPath = uploadDir.toFile().getAbsolutePath();
-
-        if (dirName.startsWith("../")) dirName = dirName.replace("../", "");
-
-        registry.addResourceHandler("/" + dirName + "/**").addResourceLocations("file:/"+ uploadPath + "/");
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
